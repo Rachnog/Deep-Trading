b8b2cd0d7b6c3964129a34c13c936e12bb9e5b7d
Alex Honchar <rachnogstyle@gmail.com>
2017-06-06 11:14:28 +0200

    Initial commit

---
+++ time_series_plus_text/process_data.py
@@ -0,0 +1,137 @@
+import pandas as pd
+from pandas import Series,DataFrame
+import numpy as np
+import time
+from datetime import date
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem.porter import PorterStemmer
+
+import cPickle
+
+from gensim.models import Word2Vec
+import logging
+
+logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s')
+
+
+def load_text_csv(filename = 'Combined_News_DJIA.csv', date_split = date(2014,12,31)):
+	'''
+		Load news from csv, group them and split in train/test set due to @date_split
+	'''
+	df = pd.read_csv(filename)
+	df['Combined']=df.iloc[:,2:27].apply(lambda row: ''.join(str(row.values)), axis=1)
+
+	train = df.loc[(pd.to_datetime(df["Date"]) <= date_split),['Label','Combined']]
+	test = df.loc[(pd.to_datetime(df["Date"]) > date_split),['Label','Combined']]
+
+	return train, test
+
+
+def load_ts_csv(filename = 'DJIA_table.csv', date_split = date(2014,12,31)):	
+	'''
+		Load time series from csv, taking adjustment close prices;
+		transforming them into percentage of price change;
+		split in train/test set due to @date_split
+	'''
+	data_original = pd.read_csv(filename)[::-1]
+
+	train2 = data_original.loc[(pd.to_datetime(data_original["Date"]) <= date_split)]
+	test2 = data_original.loc[(pd.to_datetime(data_original["Date"]) > date_split)]
+
+	data_chng_train = train2.ix[:, 'Adj Close'].pct_change().dropna().tolist()
+	data_chng_test = test2.ix[:, 'Adj Close'].pct_change().dropna().tolist()
+
+	return data_chng_train, data_chng_test
+
+
+def text_process(text):
+    '''
+    Takes in a string of text, then performs the following:
+	    1. Tokenizes and removes punctuation
+	    2. Removes  stopwords
+	    3. Stems
+	    4. Returns a list of the cleaned text
+    '''
+    if pd.isnull(text):
+        return []
+    # tokenizing
+    tokenizer = RegexpTokenizer(r'\w+')
+    text_processed=tokenizer.tokenize(text)
+    
+    # removing any stopwords
+    text_processed = [word.lower() for word in text_processed if word.lower() not in stopwords.words('english')]
+    
+    # steming
+    porter_stemmer = PorterStemmer()
+    
+    text_processed = [porter_stemmer.stem(word) for word in text_processed]
+    
+    try:
+        text_processed.remove('b')
+    except: 
+        pass
+
+    return " ".join(text_processed)
+
+
+def transform_text2sentences(train, test, save_train = 'train_text.p', save_test = 'test_text.p'):
+	train_text = []
+	test_text = []
+	for each in train['Combined']:
+	    train_text.append(text_process(each))
+	for each in test['Combined']:
+	    test_text.append(text_process(each))
+
+	if save_train != None: cPickle.dump(train_text, open(save_train, 'wb')) 
+	if save_test != None: cPickle.dump(test_text, open(save_test, 'wb')) 
+
+	return train_text, test_text
+
+
+def transform_text_into_vectors(train_text, test_text, embedding_size = 100, model_path = 'word2vec10.model'):
+	data_for_w2v = []
+	for text in train_text + test_text:
+	    words = text.split(' ')
+	    data_for_w2v.append(words)
+
+	model = Word2Vec(data_for_w2v, size=embedding_size, window=5, min_count=1, workers=4)
+	model.save(model_path)
+	model = Word2Vec.load(model_path)
+
+	train_text_vectors = [[model[x] for x in sentence.split(' ')] for sentence in train_text]
+	test_text_vectors = [[model[x] for x in sentence.split(' ')] for sentence in test_text]
+
+	train_text_vectors = [np.mean(x, axis=0) for x in train_text_vectors]
+	test_text_vectors = [np.mean(x, axis=0) for x in test_text_vectors]
+
+	return train_text_vectors, test_text_vectors, model
+
+
+def split_into_XY(data_chng_train, train_text_vectors, step, window, forecast):
+	X_train, X_train_text, Y_train = [], [], []
+	for i in range(0, len(data_chng_train), step): 
+	    try:
+	        x_i = data_chng_train[i:i+window]
+	        y_i = data_chng_train[i+window+forecast]  
+
+	        # text_average = np.mean(train_text_vectors[i:i+WINDOW], axis=0)
+	        text_average = train_text_vectors[i:i+window]
+
+	        last_close = x_i[-1]
+	        next_close = y_i
+
+	        if y_i > 0.:
+	            y_i = [1, 0]
+	        else:
+	            y_i = [0, 1] 
+
+	    except Exception as e:
+	        break
+
+	    X_train.append(x_i)
+	    X_train_text.append(text_average)
+	    Y_train.append(y_i)
+
+	X_train, X_train_text, Y_train = np.array(X_train), np.array(X_train_text), np.array(Y_train)
+	return X_train, X_train_text, Y_train
---
+++ time_series_plus_text/train_nets.py
@@ -0,0 +1,99 @@
+from process_data import *
+
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.normalization import BatchNormalization
+from keras.layers import Merge
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, CSVLogger, EarlyStopping
+from keras.optimizers import RMSprop, Adam, SGD, Nadam
+from keras.layers.advanced_activations import *
+from keras.layers import Convolution1D, MaxPooling1D, AtrousConvolution1D
+from keras.layers.recurrent import LSTM, GRU
+from keras import regularizers
+
+#Visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import theano
+theano.config.compute_test_value = "ignore"
+
+
+def neural_network(window, embedding):
+	model1 = Sequential()
+	model1.add(Dense(64, input_dim=window,
+	                activity_regularizer=regularizers.l2(0.01)))
+	model1.add(BatchNormalization())
+	model1.add(LeakyReLU())
+	model1.add(Dropout(0.75))
+	model1.add(Dense(window,
+	                activity_regularizer=regularizers.l2(0.01)))
+	model1.add(BatchNormalization())
+	model1.add(LeakyReLU())
+
+
+	model2 = Sequential()
+	model2.add(LSTM(input_shape = (window, embedding,), output_dim=window, return_sequences=True, recurrent_dropout=0.75))   
+	model2.add(LSTM(output_dim=window, return_sequences=False, recurrent_dropout=0.75))                               
+
+	merged = Merge([model1, model2], mode='sum')
+
+	final_model = Sequential()
+	final_model.add(merged)
+
+	final_model.add(Dense(16))
+	final_model.add(BatchNormalization())
+	final_model.add(LeakyReLU())
+
+	final_model.add(Dense(2))
+	final_model.add(Activation('softmax'))
+
+	opt = Nadam(lr=0.002)
+	final_model.compile(optimizer=opt, 
+	              loss='categorical_crossentropy',
+	              metrics=['accuracy'])
+
+	return final_model
+
+
+train, test = load_text_csv()
+data_chng_train, data_chng_test = load_ts_csv()
+
+# train_text, test_text = transform_text2sentences(train, test)
+train_text = cPickle.load(open('train_text.p', 'rb'))[1:]
+test_text = cPickle.load(open('test_text.p', 'rb'))[1:]
+
+train_text_vectors, test_text_vectors, model = transform_text_into_vectors(train_text, test_text, 100)
+
+X_train, X_train_text, Y_train = split_into_XY(data_chng_train, train_text_vectors, 1, 10, 1)
+X_test, X_test_text, Y_test = split_into_XY(data_chng_test, test_text_vectors, 1, 10, 1)
+
+final_model = neural_network(10, 100)
+reduce_lr = ReduceLROnPlateau(monitor='val_acc', factor=0.9, patience=50, min_lr=0.000001, verbose=1)
+checkpointer = ModelCheckpoint(filepath="test.hdf5", verbose=1, save_best_only=True)
+
+history = final_model.fit([X_train, X_train_text], Y_train, 
+          nb_epoch = 500, 
+          batch_size = 128, 
+          verbose=1, 
+          validation_data = ([X_test, X_test_text], Y_test),
+          callbacks=[reduce_lr, checkpointer],
+          shuffle=True)
+
+plt.figure()
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='best')
+plt.show()
+
+plt.figure()
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='best')
+plt.show()
